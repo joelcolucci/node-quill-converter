446b8f1cf1f3193acdea999a4fd1dcb4c1e19586
joelcolucci <joelcolucci@gmail.com>
2018-09-02 08:59:21 -0400

    test: add regression test for github issue #2

--- test/node-quill-converter.test.js
+++ test/node-quill-converter.test.js
@@ -54,4 +54,14 @@ describe('node-quill-converter', () => {
 
     expect(htmlResult).toEqual(htmlExpected);
   });
+
+  it('GitHub Issue #2', () => {
+    let issueDeltaJSON = "{\"ops\":[{\"insert\":\"first\"},{\"attributes\":{\"list\":\"ordered\"},\"insert\":\"\\n\"},{\"insert\":\"second\"},{\"attributes\":{\"list\":\"ordered\"},\"insert\":\"\\n\"},{\"insert\":\"next level\"},{\"attributes\":{\"indent\":1,\"list\":\"ordered\"},\"insert\":\"\\n\"}]}"
+    let delta = JSON.parse(issueDeltaJSON);
+
+    let htmlExpected = "<ol><li>first</li><li>second</li><li class=\"ql-indent-1\">next level</li></ol>";
+    let htmlResult = convertDeltaToHtml(delta);
+
+    expect(htmlResult).toEqual(htmlExpected);
+  });
 });
